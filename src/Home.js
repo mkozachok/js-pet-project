ca8b8407f7036aa12d305a1f457d0b4b5917ccad
Mykhaylo <epocalips009@gmail.com>
2019-08-16 16:01:45 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 
+
+const HomePage = ({history}) => {
+    const handleClick = (path) => 
+        history.push(path)
+    
+    return (
+        <div class="text-center">
+           <h1>Home</h1>
+           <button type="button" class="btn btn-primary btn-lg" onClick={()=> handleClick('/login')}>Login</button>
+        </div>)
+}
+
 class Home extends React.Component {
-    handleClick(path) {
-        this.props.history.push(path);
-    }
+
 
     render() {
        return (
@@ -16,4 +26,21 @@ class Home extends React.Component {
     }
 };
 
-export default withRouter(Home);
+export default withRouter(HomePage);
+
+
+-------------------
+App.js
+import {HomePage, LoginPage,...} from './pages'
+
+/components/
+../index.js
+/pages/
+    HomePage/HomePage.js
+.../index.ts
+    import {HomePage as Component}  from './HomePage'
+    export const HomePage = withRouter(Component);
+ ../index.js
+    export {HomePage} from './HomePage'
+
+
